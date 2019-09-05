cfd941d236994a757d20da2ebad8c705749a27cc
Willem Clarke <willem.2@hotmail.com>
2019-09-06 02:38:29 +1000

    third commit - adding signup page form

--- src/app.js
+++ src/app.js
@@ -8,5 +8,9 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../assets/index.html"));
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../assets/login.html"));
+});
+
 app.listen(process.env.port || 3000);
 console.log("Running at port 3000");
