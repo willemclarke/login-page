d887f5d9269d0ff89590e4ae7495004a85cce12f
Willem Clarke <willem.2@hotmail.com>
2019-09-08 17:26:08 +1000

    not working presently, implementing writing user form data to userdata.json

--- src/app.js
+++ src/app.js
@@ -1,7 +1,39 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
+app.use(express.urlencoded());
+
+//readfile first, then write
+
+app.post("/create-user", (req, res) => {
+  const username = req.body.user_name;
+  const password = req.body.user_password;
+  console.log(username, password);
+
+  fs.readFile("../assets/userdata.json", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+
+  const userInfo = {
+    username: {
+      username,
+      password
+    }
+  };
+
+  const data = JSON.stringify(userInfo, null, 2);
+
+  fs.writeFile("../assets/userdata.json", data, err => {
+    if (err) throw err;
+    console.log("Written to file");
+  });
+  res.end();
+});
+
+// code associated with serving actual pages - index (signup-page) and (login-page)
 app.use(express.static("../assets"));
 
 app.get("/", (req, res) => {
